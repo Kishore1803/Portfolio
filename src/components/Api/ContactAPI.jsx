36527bdb749097be3cc689c88e503a40db999888
Kishore K <kannan.v4704@gmail.com>
2026-09-10 21:02:42 +0530

    update portfolio

--- src/components/Api/ContactAPI.jsx
+++ src/components/Api/ContactAPI.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8081/api/contact";
+const API_URL = "https://portfolio-backend-nqnz.onrender.com/api/contact";
 
 export const submitContactForm = async (contactData) => {
   const response = await axios.post(API_URL, contactData);
